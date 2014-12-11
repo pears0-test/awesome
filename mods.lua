284bd7481deab01bbbf5c33a9b53a2b9c6e14953
Gonzo <pears0@gmail.com>
2014-12-11 19:28:31 +0000

    Added new tag symbols

--- mods.lua
+++ mods.lua
@@ -4,22 +4,6 @@
 -- I suggest you to remap Mod4 to another key using xmodmap or other tools.
 -- However, you can use another modifier like Mod1, but it may interact with others.
 
---layouts =
---{
---    awful.layout.suit.floating,
---    awful.layout.suit.tile,
---    awful.layout.suit.tile.left,
---    awful.layout.suit.tile.bottom,
---    awful.layout.suit.tile.top,
---    awful.layout.suit.fair,
---    awful.layout.suit.fair.horizontal,
---    awful.layout.suit.spiral,
---    awful.layout.suit.spiral.dwindle,
---    awful.layout.suit.max,
---    awful.layout.suit.max.fullscreen,
---    awful.layout.suit.magnifier
---}
-
 modkey = "Mod4"
 
 -- Global Keys
